74dc98659d76dc6b4453c3253a0bf7bb0bfe850a
Patryk360 <nexigamingyt@gmail.com>
2025-11-10 13:06:45 +0100

    fix

--- src/main/kotlin/pl/dewersee/herobrine/HerobrinePlugin.kt
+++ src/main/kotlin/pl/dewersee/herobrine/HerobrinePlugin.kt
@@ -1,16 +1,26 @@
 package pl.dewersee.herobrine
 
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+import org.bukkit.command.SimpleCommandMap
 import org.bukkit.plugin.java.JavaPlugin
 import pl.dewersee.herobrine.commands.HelloCommand
+import org.bukkit.command.Command as BukkitCommand
 
 class HerobrinePlugin : JavaPlugin() {
 
     override fun onEnable() {
-        logger.info("Herobrine plugin został włączony!");
-        getCommand("hello")?.setExecutor(HelloCommand())
+        logger.info("Herobrine plugin został włączony!")
+
+        // Rejestracja komendy programowo
+        server.commandMap.register("herobrine", object : BukkitCommand("hello") {
+            override fun execute(sender: CommandSender, label: String, args: Array<out String>): Boolean {
+                return HelloCommand().onCommand(sender, this, label, args)
+            }
+        })
     }
 
     override fun onDisable() {
-        logger.info("Herobrine plugin został wyłączony!");
+        logger.info("Herobrine plugin został wyłączony!")
     }
 }
